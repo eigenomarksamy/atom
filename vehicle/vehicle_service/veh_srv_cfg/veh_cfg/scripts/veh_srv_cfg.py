5e8dc8fcb6d60b4a4ca529a429b789d567c9a673
Omar <omarkhaledauto>
2020-11-14 13:22:49 +0200

    add scripts for odom_gt and cfg

--- vehicle/vehicle_service/veh_srv_cfg/veh_cfg/scripts/veh_srv_cfg.py
+++ vehicle/vehicle_service/veh_srv_cfg/veh_cfg/scripts/veh_srv_cfg.py
@@ -1,9 +1,16 @@
 #! /usr/bin/env python
 
 class VehSrvCfg:
-    def __init__(self, vehName, vehId, vehNs):
+    def __init__(self, vehNs, vehName=None, vehId=0):
         self._vehName   = vehName
         self._vehId     = vehId
         self._vehNs     = vehNs
-        self._gazebo_model_name = self._vehNs
-        self._gazebo_frame_id   = '/' + self._vehNs + '/base_link'
+        self._gaz_model_name = self._vehNs
+        self._gaz_frame_id   = '/' + self._vehNs + '/base_link'
+
+class VehSrvCfgOdomGt(VehSrvCfg):
+    def __init__(self, vehNs, vehName=None, vehId=0):
+        VehSrvCfg.__init__(self, vehNs, vehName, vehId)
+        self._node_name = self._vehNs + str(self._vehId) + '_odom_gt'
+        self._topic_name = self._vehNs + str(self._vehId) + '/odom/gt'
+        self._gaz_srv_name = '/gazebo/get_model_state'
---
+++ vehicle/vehicle_service/veh_srv_intf/veh_srv_gt_odom/scripts/odom_gt.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+
+# dependencies:
+#   - vehicle properties
+#   - odom_gt ros msg
+
+import rospy
+from veh_srv_ros_types.msg import VehOdomGt
+from nav_msgs.msg import Odometry
+from gazebo_msgs.srv import GetModelState, GetModelStateRequest
+
+class OdomGt:
+    def __init__(self, veh_cfg):
+        self._veh_cfg = veh_cfg
+        self._gaz_frame_id = self._veh_cfg._gaz_frame_id
+        self._gaz_model_name = self._veh_cfg._gaz_model_name
+        self._node_name = self._veh_cfg._node_name
+        self._topic_name = self._veh_cfg._topic_name
+        self._gaz_srv_name = self._veh_cfg._gaz_srv_name
+
+    def init(self):
+        rospy.init_node(self._node_name)
+        self._odom_pub = rospy.Publisher(self._topic_name, VehOdomGt, queue_size=100)
+        rospy.wait_for_service(self._gaz_srv_name)
+        self._get_model_srv = rospy.ServiceProxy(self._gaz_srv_name, GetModelState)
+        self._veh_odom_gt = VehOdomGt()
+        self._veh_odom_gt.odometry.header.frame_id = self._gaz_frame_id
+        self._model = GetModelStateRequest()
+        self._model.model_name = self._gaz_model_name
+
+    def execute(self):
+        self._result = self._get_model_srv(self._model)
+        self._veh_odom_gt.odometry.pose.pose = self._result.pose
+        self._veh_odom_gt.odometry.twist.twist = self._result.twist
+        self._veh_odom_gt.odometry.header.stamp = rospy.Time.now()
+        self._veh_odom_gt.timestamp = self._veh_odom_gt.odometry.header.stamp
+        self._odom_pub.publish(self._veh_odom_gt)
+
+class OdomGtSim(OdomGt):
+    def __init__(self, veh_cfg):
+        OdomGt.__init__(self, veh_cfg)
+        self._node_name += '_sim'
+        self._topic_name += '/sim'
+
+    def init(self):
+        rospy.init_node(self._node_name)
+        self._odom_pub = rospy.Publisher(self._topic_name, Odometry, queue_size=100)
+        rospy.wait_for_service(self._gaz_srv_name)
+        self._get_model_srv = rospy.ServiceProxy(self._gaz_srv_name, GetModelState)
+        self._veh_odom_gt = Odometry()
+        self._veh_odom_gt.header.frame_id = self._gaz_frame_id
+        self._model = GetModelStateRequest()
+        self._model.model_name = self._gaz_model_name
+
+    def execute(self):
+        self._result = self._get_model_srv(self._model)
+        self._veh_odom_gt.pose.pose = self._result.pose
+        self._veh_odom_gt.twist.twist = self._result.twist
+        self._veh_odom_gt.header.stamp = rospy.Time.now()
+        self._odom_pub.publish(self._veh_odom_gt)
