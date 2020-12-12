d82932b4340956f180012494300a9c81ebcaf78f
Omar <omarkhaledauto>
2020-12-12 15:13:40 +0200

    add gear interfacing class

--- vehicle/vehicle_service/veh_srv_intf/veh_srv_cmd/src/cmd_intf.py
+++ vehicle/vehicle_service/veh_srv_intf/veh_srv_cmd/src/cmd_intf.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 import rospy
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64, UInt8
 
 class Cmd:
     def __init__(self, sub_topic_name, pub_topic_name):
@@ -16,7 +16,7 @@ class Cmd:
         self._rate_obj = rospy.Rate(self._rate_val)
 
     def set_rate_val(self, rate_val=100):
-        self._rate_val = 100
+        self._rate_val = rate_val
 
     def get_rate_props(self):
         return self._rate_obj, self._rate_val
@@ -29,3 +29,12 @@ class Cmd:
 
     def get_data(self):
         return self._data
+
+class GearCmd(Cmd):
+    def __init__(self, sub_topic_name, pub_topic_name):
+        Cmd.__init__(self, sub_topic_name, pub_topic_name)
+
+    def init(self):
+        self._pub = rospy.Publisher(self._pub_topic_name, UInt8, queue_size=100)
+        self._pub_msg = UInt8()
+        self._rate_obj = rospy.Rate(self._rate_val)
--- vehicle/vehicle_service/veh_srv_intf/veh_srv_cmd/src/gear_intf.py
+++ vehicle/vehicle_service/veh_srv_intf/veh_srv_cmd/src/gear_intf.py
@@ -3,34 +3,12 @@
 import sys
 import rospy
 from veh_srv_ros_types.msg import VehGear
-from std_msgs.msg import UInt8
+sys.path.append('.')
+from cmd_intf import GearCmd
 
-class Gear:
-    def __init__(self, sub_topic_name, pub_topic_name):
-        self._sub_topic_name = sub_topic_name
-        self._pub_topic_name = pub_topic_name
-        self._data = None
-        self._rate = 100
-
-    def init(self):
-        self._pub = rospy.Publisher(self._pub_topic_name, UInt8, queue_size=100)
-        self._pub_msg = UInt8()
-        self._rate_ros = rospy.Rate(self._rate)
-
-    def fill_pub_msg(self, data):
-        self._pub_msg = data
-
-    def publish(self):
-        self._pub.publish(self._pub_msg)
-
-    def set_rate(self, rate=100):
-        self._rate = rate
-
-    def get_rate(self):
-        return self._rate_ros, self._rate
-
-    def get_data(self):
-        return self._data
+class Gear(GearCmd):
+    def __init__(self, sub_topic_name, pub_topic_name=None):
+        GearCmd.__init__(self, sub_topic_name, pub_topic_name)
 
     def execute(self):
         self._data = rospy.wait_for_message(topic=self._sub_topic_name, topic_type=VehGear)
@@ -41,9 +19,8 @@ def main():
     sub_topic_name = None
     pub_topic_name = None
     obj = Gear(sub_topic_name=sub_topic_name, pub_topic_name=pub_topic_name)
-    obj.set_rate()
     obj.init()
-    rate_obj, _ = obj.get_rate()
+    rate_obj, _ = obj.get_rate_props()
     while not rospy.is_shutdown:
         obj.execute()
         rate_obj.sleep()
