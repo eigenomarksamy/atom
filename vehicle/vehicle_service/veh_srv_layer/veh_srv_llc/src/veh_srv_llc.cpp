9a9f734fa803e425da97dbe48bc1f0691da28e5e
Omar <omarkhaledauto>
2020-12-27 23:44:56 +0200

    remove types and add initiali subscriber

--- vehicle/vehicle_service/veh_srv_layer/veh_srv_llc/src/veh_srv_llc.cpp
+++ vehicle/vehicle_service/veh_srv_layer/veh_srv_llc/src/veh_srv_llc.cpp
@@ -3,12 +3,9 @@
  * Date:    27-12-2020
  */
 
-/* language-based inclusion */
-#include <stdio.h>
-#include <iostream>
-
 /* ros standard libraries inclusion */
 #include "ros/ros.h"
+#include "std_msgs/String.h"
 
 /* defined msg and srv types inclusion */
 
@@ -16,8 +13,21 @@
 /* pkg-level header inclusion */
 #include "srv_llc_type.h"
 
+/* language-based inclusion */
+#include <stdio.h>
+#include <iostream>
+
+void callback(const std_msgs::String::ConstPtr& msg)
+{
+    ROS_INFO("I heard: [%s]", msg->data.c_str());
+}
+
 int32_t main(int argc, char **argv)
 {
-    std::cout << "Hello World" << std::endl;
+    /* initialize ros node */
+    ros::init(argc, argv, "node_name");
+    ros::NodeHandle n;
+    ros::Subscriber sub = n.subscribe("topic_name", 1000, callback);
+    ros::spin();
     return 0;
 }
