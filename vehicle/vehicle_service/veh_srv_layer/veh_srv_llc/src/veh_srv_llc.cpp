9991b97f4d333ce9ddb54138f7b88cc2d7ba42b0
Omar K. Samy <eigenomarksamy@github.com>
2020-12-27 21:54:46 +0200

    add types header and include ros header

--- vehicle/vehicle_service/veh_srv_layer/veh_srv_llc/src/veh_srv_llc.cpp
+++ vehicle/vehicle_service/veh_srv_layer/veh_srv_llc/src/veh_srv_llc.cpp
@@ -1,7 +1,22 @@
+/* 
+ * Author:  Omar K. Samy
+ * Date:    27-12-2020
+ */
+
+/* language-based inclusion */
 #include <stdio.h>
 #include <iostream>
 
-int main(int argc, char **argv)
+/* ros standard libraries inclusion */
+#include "ros/ros.h"
+
+/* defined msg and srv types inclusion */
+
+
+/* pkg-level header inclusion */
+#include "srv_llc_type.h"
+
+int32_t main(int argc, char **argv)
 {
     std::cout << "Hello World" << std::endl;
     return 0;
