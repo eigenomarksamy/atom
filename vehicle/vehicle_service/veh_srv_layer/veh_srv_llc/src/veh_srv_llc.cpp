7a2dd8ff38ac9210e9433c4bad808b44fa4d6ab5
Omar <omarkhaledauto>
2020-12-29 19:13:50 +0200

    implement veh_srv_llc subscriber and msg filler

--- vehicle/vehicle_service/veh_srv_layer/veh_srv_llc/src/veh_srv_llc.cpp
+++ vehicle/vehicle_service/veh_srv_layer/veh_srv_llc/src/veh_srv_llc.cpp
@@ -14,44 +14,105 @@
 #include "veh_srv_llc.h"
 
 /* language-based inclusion */
-#include <stdio.h>
 #include <iostream>
+#include <limits>
+
+/* defines */
+#define PI          M_PI
+#define STEER_RATIO (17.3f)
+#define MAX_STEER   2*PI
+#define MIN_STEER   -2*PI
+
+/* global variables */
+static struct cmd_in_data_S st_cmd_in_data;
 
 /* local function prototypes */
 void audiCmdCallback(const veh_srv_ros_types::AudiCmd::ConstPtr&);
-static void fillComCfg(const struct ros_com_S*);
-static void initRos(ros::Subscriber&, struct ros_com_cfg_S, int, char **);
+static void fillComCfg(struct ros_com_S*);
+static void initRos(ros::Subscriber&, ros::NodeHandle&, struct ros_com_cfg_S, int, char **);
+static void fillCmdOut(struct cmd_out_data_S*, struct cmd_in_data_S);
 
+/* implementation of local functions */
 void audiCmdCallback(const veh_srv_ros_types::AudiCmd::ConstPtr& msg)
 {
-    
+    st_cmd_in_data.time = msg->timestamp.toSec();
+    st_cmd_in_data.seq_counter = msg->seq_counter;
+    st_cmd_in_data.throt_in_cmd = static_cast<uint64_t>(msg->throttle_cmd);
+    st_cmd_in_data.brake_in_cmd = static_cast<uint64_t>(msg->brake_cmd);
+    st_cmd_in_data.steer_in_cmd = static_cast<sint64_t>(msg->steering_cmd);
+    st_cmd_in_data.gear_in_cmd = static_cast<Gear_CMD_E>(msg->gear_cmd);
 }
 
-static void fillComCfg(struct ros_com_cfg_S* ros_com_cfg)
+static void fillComCfg(struct ros_com_cfg_S* p_rosComCfg)
 {
-    ros_com_cfg->nodeName = "audibot_veh_llc_node";
-    ros_com_cfg->topicInName = "/audibot/vehicle_srv/llc";
-    ros_com_cfg->topicOutNames[VEH_THROTTLE] = "/audibot/vehicle_srv/veh_throttle";
-    ros_com_cfg->topicOutNames[VEH_BRAKE] = "/audibot/vehicle_srv/veh_brake";
-    ros_com_cfg->topicOutNames[VEH_STEERING] = "/audibot/vehicle_srv/veh_steering";
-    ros_com_cfg->topicOutNames[VEH_GEAR] = "/audibot/vehicle_srv/veh_gear";
-    ros_com_cfg->maxBuffLen = 1000;
+    p_rosComCfg->nodeName = "audibot_veh_llc_node";
+    p_rosComCfg->topicInName = "/audibot/vehicle_srv/llc";
+    p_rosComCfg->topicOutNames[VEH_THROTTLE] = "/audibot/vehicle_srv/veh_throttle";
+    p_rosComCfg->topicOutNames[VEH_BRAKE] = "/audibot/vehicle_srv/veh_brake";
+    p_rosComCfg->topicOutNames[VEH_STEERING] = "/audibot/vehicle_srv/veh_steering";
+    p_rosComCfg->topicOutNames[VEH_GEAR] = "/audibot/vehicle_srv/veh_gear";
+    p_rosComCfg->maxBuffLen = 1000;
+    p_rosComCfg->rosRate = 100;
 }
 
-static void initRos(ros::Subscriber& sub, struct ros_com_cfg_S ros_com_cfg, int argc, char **argv)
+static void initRos(ros::Subscriber& sub, ros::NodeHandle& node_handler,
+                    struct ros_com_cfg_S ros_com_cfg, int argc, char **argv)
 {
     ros::init(argc, argv, ros_com_cfg.nodeName);
-    ros::NodeHandle node_handle;
-    node_handle.subscribe(ros_com_cfg.topicInName, ros_com_cfg.maxBuffLen, audiCmdCallback);
+    node_handler.subscribe(ros_com_cfg.topicInName, ros_com_cfg.maxBuffLen, audiCmdCallback);
 }
 
+static void fillCmdOut(struct cmd_out_data_S* p_cmdOutData, struct cmd_in_data_S cmdInData)
+{
+    p_cmdOutData->time = ros::Time::now().toSec();
+    bool is_movable = (cmdInData.gear_in_cmd == GEAR_R || cmdInData.gear_in_cmd == GEAR_D) ? true : false;
+    if (is_movable)
+    {
+        /* convert from uint64_t to float64 (cmd * MAX_CMD<1> / UINT64_MAX) */
+        p_cmdOutData->throt_out_cmd = static_cast<float>(cmdInData.throt_in_cmd / UINT64_MAX);
+        p_cmdOutData->brake_out_cmd = static_cast<float>(cmdInData.brake_in_cmd / UINT64_MAX);
+        if (cmdInData.steer_in_cmd > 0)
+        {
+            p_cmdOutData->steer_out_cmd = static_cast<float>((cmdInData.steer_in_cmd * MAX_STEER) / SINT16_MAX);
+        }
+        else if (cmdInData.steer_in_cmd < 0)
+        {
+            p_cmdOutData->steer_out_cmd = static_cast<float>((cmdInData.steer_in_cmd * MIN_STEER) / SINT16_MIN);
+        }
+        else /* steering_cmd == 0 */
+        {
+            p_cmdOutData->steer_out_cmd = static_cast<float>(0);
+        }
+        
+        p_cmdOutData->gear_out_cmd = static_cast<uint8_t>((cmdInData.gear_in_cmd == GEAR_R) ? 1 : 0);
+    }
+    else
+    {
+        p_cmdOutData->throt_out_cmd = static_cast<float>(0);
+        p_cmdOutData->brake_out_cmd = static_cast<float>(0);
+        p_cmdOutData->steer_out_cmd = static_cast<float>(0);
+        p_cmdOutData->gear_out_cmd = static_cast<uint8_t>(0);
+    }
+    p_cmdOutData->seq_counter++;
+}
+
+/* main function */
 int32_t main(int argc, char **argv)
 {
     struct ros_com_cfg_S ros_com_cfg;
+    struct cmd_out_data_S out_data;
+    ros::NodeHandle node_handler;
+    ros::Subscriber node_sub;
+    ros::Rate node_rate(ros_com_cfg.rosRate);
+    out_data.seq_counter = 0;
     fillComCfg(&ros_com_cfg);
     /* initialize ros node */
-    ros::Subscriber node_sub;
-    initRos(node_sub, ros_com_cfg, argc, argv);
-    ros::spin();
+    initRos(node_sub, node_handler, ros_com_cfg, argc, argv);
+    while (ros::ok())
+    {
+
+        ros::spinOnce();
+        node_rate.sleep();
+    }
     return 0;
 }
--- vehicle/vehicle_service/veh_srv_layer/veh_srv_llc/src/veh_srv_llc.h
+++ vehicle/vehicle_service/veh_srv_layer/veh_srv_llc/src/veh_srv_llc.h
@@ -16,12 +16,36 @@
 /* language-based inclusion */
 #include <string>
 
+/* typedefs */
+typedef int8_t  sint8_t;
+typedef int16_t sint16_t;
+typedef int32_t sint32_t;
+typedef int64_t sint64_t;
+
 /* definition of enums, structs, defines, etc. */
+#define SINT8_MIN           INT8_MIN
+#define SINT8_MAX           INT8_MAX
+#define SINT16_MIN          INT16_MIN
+#define SINT16_MAX          INT16_MAX
+#define SINT32_MIN          INT32_MIN
+#define SINT32_MAX          INT32_MAX
+#define SINT64_MIN          INT64_MIN
+#define SINT64_MAX          INT64_MAX
+
 #define MAX_STR_LEN         100u
 #define MAX_NODE_NAME_LEN   MAX_STR_LEN
 #define MAX_TOPIC_NAME_LEN  MAX_STR_LEN
 
-enum cmd_output_E {
+enum Gear_CMD_E {
+    GEAR_NONE = 0,
+    GEAR_P,
+    GEAR_R,
+    GEAR_N,
+    GEAR_D,
+    GEAR_CMD_LEN
+};
+
+enum cmd_sys_E {
     VEH_NONE = 0,
     VEH_THROTTLE,
     VEH_BRAKE,
@@ -35,6 +59,25 @@ struct ros_com_cfg_S {
     std::string topicInName;
     std::string topicOutNames[VEH_CMD_LEN];
     uint16_t maxBuffLen;
+    uint8_t rosRate;
+};
+
+struct cmd_in_data_S {
+    double      time;           /* ros::Time::now().toSec() */
+    uint64_t    seq_counter;
+    sint64_t    steer_in_cmd;   /* from SINT64_MIN to SINT64_MAX */
+    uint64_t    throt_in_cmd;   /* from UINT64_MIN to UINT64_MAX */
+    uint64_t    brake_in_cmd;   /* from UINT64_MIN to UINT64_MAX */
+    Gear_CMD_E  gear_in_cmd;
+};
+
+struct cmd_out_data_S {
+    double      time;           /* ros::Time::now().toSec() */
+    uint64_t    seq_counter;
+    float       steer_out_cmd;   /* from SINT64_MIN to SINT64_MAX */
+    float       throt_out_cmd;   /* from UINT64_MIN to UINT64_MAX */
+    float       brake_out_cmd;   /* from UINT64_MIN to UINT64_MAX */
+    uint8_t     gear_out_cmd;
 };
 
 #endif /* VEH_SRV_LLC_H_ */
